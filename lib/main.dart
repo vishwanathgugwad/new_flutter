7decc99ec0bb97da6069ed0e1ca79f1f64611622
vishwanathgugwad <vishwanath.gugwad@gmail.com>
2021-09-04 21:56:31 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
+import 'pages/home_page.dart';
+import 'pages/login_page.dart';
 void main(){
   runApp(MyApp());
 
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+ // const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-    home:Material(
-      child: Center(
-        child: Container(
-          child: Text("Welcome to my app "),
-        ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
-    ),
+
+      routes: {
+      "/": (context)=> LoginPage(),
+        "home":(context)=>HomePage(),
+        "/login":(context)=> LoginPage(),
+
+      },
     );
   }
 }
