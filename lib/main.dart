d4c9880c6a0618f0a17a0b350fe0739f9f97b45b
vishwanathgugwad <vishwanath.gugwad@gmail.com>
2021-09-05 12:21:42 +0530

    day 5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
@@ -24,8 +25,9 @@ class MyApp extends StatelessWidget {
 
       routes: {
       "/": (context)=> LoginPage(),
-        "home":(context)=>HomePage(),
-        "/login":(context)=> LoginPage(),
+        MyRoutes.homeRoute: (context)=>HomePage(),
+        MyRoutes.loginRoute: (context)=>LoginPage(),
+
 
       },
     );
