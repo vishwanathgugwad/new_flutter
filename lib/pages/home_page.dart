601b87009d250621faf60c6eea3ad01bc2c12fa1
vishwanathgugwad <vishwanath.gugwad@gmail.com>
2021-09-06 18:38:33 +0530

    day 8 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -45,14 +45,41 @@ class _HomePageState extends State<HomePage> {
 
       ),
       drawer: MyDrawer(),
-      body: (CatalogModel!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
-          itemCount:  CatalogModel.items.length,
-        itemBuilder:  (context,index){
-          return ItemWidget (item:   CatalogModel.items[index],);
-        },
+      body: (CatalogModel!=null && CatalogModel.items.isNotEmpty)?
+      GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+          mainAxisSpacing: 16,
+          crossAxisSpacing: 16),
+          itemBuilder: (context,index){
+            final item = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
 
+                child: GridTile(
+                  header: Container(
+                    padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple
+                      ),
+                      child: Text(item.name,style: TextStyle(
+                        color: Colors.white
+                      ),)),
+                    footer: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                            color: Colors.black
+                        ),
+                        child: Text(item.price.toString(),style: TextStyle(
+                            color: Colors.white
+                        ),)),
+                    child: Image.network(item.image,))
 
-      ):Center(
+            );
+          },
+          itemCount: CatalogModel.items.length
+      )
+          :Center(
         child: CircularProgressIndicator(),
       ),
 
